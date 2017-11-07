b62cb3e9455000663695e7db02c18c4172f24031
Daniil <thepoddubstep@gmail.com>
2017-11-07 16:45:00 +0300

    Improved security in questions controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,11 +1,7 @@
 class QuestionsController < ApplicationController
   before_action :authorize_user, except: [:create]
-  before_action :check_question, only: [:edit, :update]
+  before_action :question_edit, only: [:edit, :update, :destroy]
   before_action :set_question, only: [:edit, :update, :destroy]
-  # Showing all questions without answers
-  def index
-    @questions = Question.where(user_id: session[:user_id], answer: nil)
-  end
 
   def create
     @question = Question.new
@@ -18,52 +14,40 @@ class QuestionsController < ApplicationController
       redirect_to root_url
     end
   end
-
-  # Form for answering question
-  def edit
+  # Showing all questions without answers
+  def index
+    @questions = Question.where(user_id: session[:user_id], answer: nil)
   end
-
   # Answering question
   def update
-    @question.answer = question_update[:answer]
+    @question.answer = question_params[:answer]
     if @question.save
       redirect_to user_path(@question.user), notice: 'Question successfully answered!'
     else
       redirect_to edit_question_path(@question)
     end
   end
-
   # Deleting question
   def destroy
-    delete_question
-    user = @question.user
     @question.destroy
-    redirect_to user_path(user), notice: 'Question deleted'
+    redirect_to questions_path, notice: 'Question deleted'
   end
 
   private
-    # Finding question by id
-    def set_question
-      @question = Question.find(params[:id])
-    end
-    # Checking question for delete
-    def delete_question
-      reject_user unless current_user.present? && current_user.id == set_question.user_id
-    end
-    # Checking permission for editing
-    def check_question
-      reject_user unless current_user.present? && current_user.id == set_question.user_id && (set_question.answer == nil || set_question.answer == "")
-    end
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def question_params
-      params.require(:question).permit(:user_id, :text, :answer)
-    end
-    # Permitted params for update
-    def question_update
-      params.require(:question).permit(:answer)
-    end
-    # Checking authorize user
-    def authorize_user
-      reject_user unless current_user.present?
-    end
+  # Permitted params
+  def question_params
+    params.require(:question).permit(:user_id, :text, :answer, :id)
+  end
+  # Setting up question
+  def set_question
+    @question = Question.find(params[:id])
+  end
+  # Edit question (update, delete), checking authorization
+  def question_edit
+    reject_user if current_user.questions.where(id: params[:id]).empty?
+  end
+  # Authorizing user
+  def authorize_user
+    reject_user unless current_user.present?
+  end
 end
